f52007dfadafaf67a20b4b1ff4f17bb9407e7eac
Cortina17 <diego.cortinagarcia@gmail.com>
2022-02-19 23:15:34 +0100

    solved bug
    checkbox status changed when enter in edit mode

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -25,9 +25,13 @@ export default {
 	},
 	data() {
 		return {
-			isDone: this.done,
 			isEditing: false,
 		};
+	},
+	computed:{
+		isDone() {
+			return this.done;
+		}
 	},
 	methods:{
 		deleteToDo(){
