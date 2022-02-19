276449a04fee141ac5ec40d8530ae4607c64258c
Cortina17 <diego.cortinagarcia@gmail.com>
2022-02-19 20:51:47 +0100

    form created
    now the list is able to render new inputs, and clear the input bar at the same time. also, prevent to add empty items to the list

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <h1>To Do List</h1>
-    <to-do-form></to-do-form>
+    <to-do-form @todo-added="addToDo"></to-do-form>
     <ul>
       <li v-for="item in MockTodoItems" :key="item.id">
         <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
@@ -30,6 +30,11 @@ export default {
         { id: uniqueId('todo-'), label: 'Learn to use the keyboard without looking', done:false},
         { id: uniqueId('todo-'), label: 'Have fun', done:false},
       ]
+    };
+  },
+  methods: {
+    addToDo(toDoLabel){
+      this.MockTodoItems.push({id:uniqueId('todo-'), label: toDoLabel, done: false});
     }
   }
 }
--- src/components/ToDoForm.vue
+++ src/components/ToDoForm.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <form>
+        <form @submit.prevent="onSubmit">
             <label for="new-todo-input">What needs to be done?</label>
-            <input type="text" id="new-todo-input" name="new-todo" autocomplete="off" >
+            <input type="text" id="new-todo-input" name="new-todo" autocomplete="off" v-model.lazy.trim="label">
             <button type="submit">Add</button>
         </form>
     </div>
@@ -10,6 +10,19 @@
 
 <script>
 export default {
-    
-}
+    methods: {
+        onSubmit(){
+            if(this.label === ""){
+                return;
+            }
+            this.$emit("todo-added", this.label);
+            this.label = "";
+        }
+    },
+    data(){
+        return {
+            label: "",
+        };
+    }
+};
 </script>
